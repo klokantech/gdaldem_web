382436818c0e62108e8c2a8741cdfb064ef8464c
Václav Klusák <keo@keo.cz>
2011-05-06 13:02:55 +0200

    + multithreaded version

--- Makefile
+++ Makefile
@@ -2,9 +2,13 @@
 GDAL_CFLAGS = `gdal-config --cflags`
 GDAL_LDFLAGS = `gdal-config --libs`
 
+# comment these two out if you want single threaded version
+EXTRA_CFLAGS = -DWITH_PTHREADS
+EXTRA_LDFLAGS = -lpthread
+
 CC = gcc
-CFLAGS = -Wall -Wextra -ansi -pedantic -std=c99 -O2 $(GDAL_CFLAGS)
-LDFLAGS = $(GDAL_LDFLAGS)
+CFLAGS = -Wall -Wextra -ansi -pedantic -std=c99 -O2 $(GDAL_CFLAGS) $(EXTRA_CFLAGS)
+LDFLAGS = $(GDAL_LDFLAGS) $(EXTRA_LDFLAGS)
 
 all: gdal_z2rgb
 
--- gdal_z2rgb.c
+++ gdal_z2rgb.c
@@ -26,12 +26,48 @@
 #include <cpl_string.h>
 #include <gdal.h>
 
+#ifdef WITH_PTHREADS
+#include <pthread.h>
+#endif
+
 static char *USAGE =
     "USAGE: gdal_z2rgb -help | OPTIONS src dst\n"
     "OPTIONS: [-b band] [-co \"NAME=VALUE\"] [-nodata own|num num]\n"
-    "         [-of format] [-scale min max] [-r resolution]";
+    "         [-of format] [-P num] [-scale min max] [-r resolution]";
+
+/* options */
+static float resolution = 1.0f;
+static float src_nodata = -FLT_MAX;
+static int dst_nodata = 0;
+static int has_nodata = 0;
+static int scale_min = -12000;
+static int scale_max = 10000;
+
+/* variables */
+static float scale_range;
+
+/* resources */
+#ifdef WITH_PTHREADS
+typedef struct {
+    float *src;
+    int area;
+    int go;
+    pthread_t thread;
+    unsigned char *dst[3];
+} CTX;
+
+static int wait_sem;
+static pthread_mutex_t go_mutex;
+static pthread_mutex_t wait_mutex;
+static pthread_cond_t go_cond;
+static pthread_cond_t wait_cond;
+#endif
 
 static void calculate_buf_size(GDALDatasetH, int, int*, int*);
+static void z2rgb(float*, unsigned char*[3], int);
+#ifdef WITH_PTHREADS
+static void *thread_run(void*);
+#endif
 
 int
 main(int argc, char *argv[])
@@ -41,14 +77,11 @@ main(int argc, char *argv[])
     char **dst_opts = NULL;
     char *dst_path;
     char *src_path;
-    float resolution = 1.0f;
-    float src_nodata = -FLT_MAX;
     int band_no = 1;
-    int dst_nodata = 0;
-    int has_nodata = 0;
-    int scale_min = -12000;
-    int scale_max = 10000;
     int user_nodata = 0;
+#ifdef WITH_PTHREADS
+    int n_threads;
+#endif
 
     /* resources */
     int ret = 1;
@@ -56,24 +89,36 @@ main(int argc, char *argv[])
     GDALDatasetH dst = NULL;
     GDALDatasetH src = NULL;
     unsigned char *dst_buf[3] = {NULL, NULL, NULL};
+#ifdef WITH_PTHREADS
+    CTX *contexts = NULL;
+    pthread_attr_t thread_attr;
+#endif
 
     /* variables */
     CPLErr err;
     double geotrans[6];
     float n_blks;
-    float scale_range;
     GDALDriverH drv;
-    GDALRasterBandH dst_band;
     GDALRasterBandH src_band;
     int blk_area;
     int blk_x_size;
     int blk_y_size;
-    int buf_area;
-    int buf_x_size;
-    int buf_y_size;
     int ds_x_size;
     int ds_y_size;
     int i, n, x, y;
+#ifdef WITH_PTHREADS
+    CTX *ctx;
+#endif
+
+#ifdef WITH_PTHREADS
+    pthread_mutex_init(&go_mutex, NULL);
+    pthread_mutex_init(&wait_mutex, NULL);
+    pthread_cond_init(&go_cond, NULL);
+    pthread_cond_init(&wait_cond, NULL);
+    pthread_attr_init(&thread_attr);
+    pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
+    n_threads = sysconf(_SC_NPROCESSORS_ONLN);
+#endif
 
     i = 1;
     while (i < argc) {
@@ -107,7 +152,12 @@ main(int argc, char *argv[])
             scale_min = atoi(argv[i + 1]);
             scale_max = atoi(argv[i + 2]);
             i += 3;
-       } else {
+#ifdef WITH_PTHREADS
+        } else if (strcmp(argv[i], "-P") == 0 && i + 1 < argc) {
+            n_threads = atoi(argv[i + 1]);
+            i += 2;
+#endif
+        } else {
             break;
         }
     }
@@ -135,6 +185,12 @@ main(int argc, char *argv[])
         fprintf(stderr, "%s: File exists\n", dst_path);
         goto end;
     }
+#ifdef WITH_PTHREADS
+    if (n_threads < 1) {
+        fprintf(stderr, "Invalid number of threads: %d\n", n_threads);
+        goto end;
+    }
+#endif
 
     GDALAllRegister();
     drv = GDALGetDriverByName(drv_name);
@@ -199,11 +255,28 @@ main(int argc, char *argv[])
     if (has_nodata)
         printf("NODATA\t%f -> %d m\n", src_nodata, dst_nodata);
 
+#ifdef WITH_PTHREADS
+    printf("THREADS\t%d\n", n_threads);
+    contexts = malloc(sizeof(CTX) * n_threads);
+    if (contexts == NULL) {
+        perror("malloc");
+        goto end;
+    }
+    for (i = 0; i < n_threads; i++) {
+        ctx = &contexts[i];
+        ctx->go = 0;
+        if (pthread_create(&ctx->thread, &thread_attr, thread_run, ctx) != 0)
+            abort();
+    }
+#endif
+
     n = 0;
     for (y = 0; y < ds_y_size; y += blk_y_size)
     for (x = 0; x < ds_x_size; x += blk_x_size) {
         int area, x_size, y_size;
-
+#ifdef WITH_PTHREADS
+        int thread_area;
+#endif
         x_size = MIN(ds_x_size, x + blk_x_size) - x;
         y_size = MIN(ds_y_size, y + blk_y_size) - y;
         area = x_size * y_size;
@@ -218,18 +291,32 @@ main(int argc, char *argv[])
 
         dst_buf[1] = dst_buf[0] + area;
         dst_buf[2] = dst_buf[1] + area;
-        for (i = 0; i < area; i++) {
-            float e, v;
-            e = src_buf[i];
-            if (has_nodata && e == src_nodata)
-                e = dst_nodata;
-            else
-                e *= resolution;
-            v = 256.0f * (e - scale_min) / scale_range;
-            dst_buf[0][i] = floor(v);
-            dst_buf[1][i] = ((int) floor(v * 256.0f)) % 256;
-            dst_buf[2][i] = 0;
+
+#ifdef WITH_PTHREADS
+        thread_area = area / n_threads;
+        wait_sem = n_threads;
+        pthread_mutex_lock(&go_mutex);
+        for (i = 0; i < n_threads; i++) {
+            int off;
+            off = thread_area * i;
+            ctx = &contexts[i];
+            ctx->area = MIN(area, off + thread_area) - off;
+            ctx->src = src_buf + off;
+            ctx->dst[0] = dst_buf[0] + off;
+            ctx->dst[1] = dst_buf[1] + off;
+            ctx->dst[2] = dst_buf[2] + off;
+            ctx->go = 1;
         }
+        pthread_cond_broadcast(&go_cond);
+        pthread_mutex_unlock(&go_mutex);
+
+        pthread_mutex_lock(&wait_mutex);
+        while (wait_sem != 0)
+            pthread_cond_wait(&wait_cond, &wait_mutex);
+        pthread_mutex_unlock(&wait_mutex);
+#else
+        z2rgb(src_buf, dst_buf, area);
+#endif
 
         err = GDALDatasetRasterIO(dst, GF_Write, x, y, x_size, y_size,
                                   dst_buf[0], x_size, y_size,
@@ -244,6 +331,18 @@ main(int argc, char *argv[])
         GDALTermProgress(n / n_blks, NULL, NULL);
     }
 
+#ifdef WITH_PTHREADS
+    pthread_mutex_lock(&go_mutex);
+    for (i = 0; i < n_threads; i++)
+        contexts[i].go = -1;
+    pthread_cond_broadcast(&go_cond);
+    pthread_mutex_unlock(&go_mutex);
+    for (i = 0; i < n_threads; i++) {
+        if (pthread_join(contexts[i].thread, NULL) != 0)
+            abort();
+    }
+#endif
+
     ret = 0;
 end:
     if (src != NULL)
@@ -253,6 +352,14 @@ end:
     CSLDestroy(dst_opts);
     free(src_buf);
     free(dst_buf[0]);
+#ifdef WITH_PTHREADS
+    pthread_mutex_destroy(&go_mutex);
+    pthread_mutex_destroy(&wait_mutex);
+    pthread_cond_destroy(&go_cond);
+    pthread_cond_destroy(&wait_cond);
+    pthread_attr_destroy(&thread_attr);
+    free(contexts);
+#endif
     return ret;
 }
 
@@ -290,4 +397,52 @@ calculate_buf_size(GDALDatasetH ds, int max_area, int *x_size, int *y_size)
     *x_size = buf_x_size;
     *y_size = buf_y_size;
 }
+
+static void
+z2rgb(float *src, unsigned char *dst[3], int area)
+{
+    int i;
+    float e, v;
+
+    for (i = 0; i < area; i++) {
+        e = src[i];
+        if (has_nodata && e == src_nodata)
+            e = dst_nodata;
+        else
+            e *= resolution;
+        v = 256.0f * (e - scale_min) / scale_range;
+        dst[0][i] = floor(v);
+        dst[1][i] = ((int) floor(v * 256.0f)) % 256;
+        dst[2][i] = 0;
+    }
+}
+
+#ifdef WITH_PTHREADS
+static void *thread_run(void *data)
+{
+    CTX *ctx = data;
+
+    for (;;) {
+        pthread_mutex_lock(&go_mutex);
+        while (!ctx->go)
+            pthread_cond_wait(&go_cond, &go_mutex);
+        if (ctx->go == -1) {
+            pthread_mutex_unlock(&go_mutex);
+            break;
+        }
+        ctx->go = 0;
+        pthread_mutex_unlock(&go_mutex);
+
+        z2rgb(ctx->src, ctx->dst, ctx->area);
+
+        pthread_mutex_lock(&wait_mutex);
+        wait_sem--;
+        if (wait_sem == 0)
+            pthread_cond_signal(&wait_cond);
+        pthread_mutex_unlock(&wait_mutex);
+    }
+
+    pthread_exit(NULL);
+}
+#endif
 
